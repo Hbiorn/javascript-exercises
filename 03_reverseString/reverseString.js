035c2ff9289343412b124c56c9cff1e76079191f
Ursa <haley.biorn@gmail.com>
2022-05-30 12:17:43 -0600

    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    return string.split('').reverse().join('');
+}
 
-};
+reverseString('hello world')
+
 
 // Do not edit below this line
 module.exports = reverseString;
